94bfbc8823f136a2692911d737b6ee7c80ac72c0
vruizsan <vruizsan@everis.com>
2021-10-04 11:41:07 +0200

    Hola Mundo

---
+++ ProyectoJava/src/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author VictorRuizSanchez
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mundo");
+	}
+
+}
